8ff95139b906d2d729ae59432c6649e690ead6c7
SP_Penguin <abrcdf1023@gmail.com>
2019-10-06 21:04:55 +0800

    update main for better api

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
-use actix_web::{web, App, HttpServer, Result, HttpResponse, Responder, get};
+use actix_web::{web, App, HttpServer, Result, HttpResponse, get};
 use listenfd::ListenFd;
 use serde::{Deserialize, Serialize};
 use std::process::Command;
 
 #[derive(Serialize, Deserialize)]
-struct MyObj {
-    name: String,
+struct Chinese {
+    text: String,
 }
 
-fn call_python(value:&str) -> String {
-    let mut translate = Command::new("python3");
-    translate.arg("run_nn.py").arg("translate").arg(value);
-    translate.current_dir("/Users/liyanxin/Life/myprojects/Chinese2English_Seq2Seq_Attention");
-    let output = translate.output().expect("failed to execute process");
+fn exec_translate_from_python(value:&str) -> String {
+    let mut _translate = Command::new("python3");
+    _translate.arg("run_nn.py").arg("translate").arg(value);
+    _translate.current_dir("/Users/liyanxin/Life/myprojects/Chinese2English_Seq2Seq_Attention");
+    let output = _translate.output().expect("failed to execute process");
     String::from_utf8_lossy(&output.stdout).to_string()
 }
 
-fn index(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
-}
-
-#[get("/hello/{name}")]
-fn hello(obj: web::Path<MyObj>) -> Result<HttpResponse> {
-    let output = call_python(&obj.name);
-    Ok(HttpResponse::Ok().json(MyObj {
-        name: output,
+#[get("/translate")]
+fn translate(info: web::Query<Chinese>) -> Result<HttpResponse> {
+    let output = exec_translate_from_python(&info.text);
+    Ok(HttpResponse::Ok().json(Chinese {
+        text: output,
     }))
 }
 
@@ -32,9 +28,8 @@ fn main() {
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new(
         || App::new()
-            .service(web::resource("/{id}/{name}/index.html").to(index))
             .service(
-                web::scope("/api").service(hello)
+                web::scope("/api").service(translate)
             )
         );
 

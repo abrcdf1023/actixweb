805643b445bf643fc8daf2013295f5109763b020
SP_Penguin <abrcdf1023@gmail.com>
2019-10-07 23:04:14 +0800

    setup diesel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate diesel;
+extern crate actixweb;
 
+use actixweb::*;
+use self::models::*;
+use diesel::prelude::*;
 use actix_web::middleware::errhandlers::{ErrorHandlerResponse, ErrorHandlers};
 use actix_web::middleware::Logger;
 use actix_web::{dev, http, web, App, HttpServer, Result, HttpResponse, get, middleware};
@@ -42,6 +47,22 @@ fn translate(info: web::Query<Chinese>) -> Result<HttpResponse> {
 }
 
 fn main() {
+    // establish db connection
+    use self::schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts.filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
+    }
+
     // init log
     std::env::set_var("RUST_LOG", "actix_web=info,info");
     env_logger::init();
